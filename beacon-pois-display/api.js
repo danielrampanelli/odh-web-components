511d907afda6dae64071e1ae6e708f86109055b1
Daniel Rampanelli <hello@danielrampanelli.com>
2019-06-19 16:09:53 +0200

    Extract endpoint urls as constants with placeholder values for the parameters.

--- beacon-pois-display/api.js
+++ beacon-pois-display/api.js
@@ -1,5 +1,8 @@
+const BEACONS_ENDPOINT_URL = 'https://api.beacon.bz.it/v1/infos'
+const SUEDTIROL_INFO_ENDPOINT_URL = 'https://service.suedtirol.info/api/ODHActivityPoi?latitude={{LAT}}&longitude={{LNG}}&radius={{RDS}}&pagesize={{CNT}}'
+
 export async function getBeacons() {
-  let response = await fetch('https://cors.io/?https://api.beacon.bz.it/v1/infos', {
+  let response = await fetch('https://cors.io/?' + BEACONS_ENDPOINT_URL, {
     method: 'GET',
     headers: new Headers({
       'Accept': 'application/json'
@@ -12,7 +15,13 @@ export async function getBeacons() {
 }
 
 export async function getNearestPOI(latitude, longitude) {
-  let response = await fetch('https://cors.io/?https://service.suedtirol.info/api/ODHActivityPoi?pagesize=1&latitude=' + latitude + '&longitude=' + longitude + '&radius=50', {
+  var url = SUEDTIROL_INFO_ENDPOINT_URL
+  url = url.replace('{{LAT}}', latitude)
+  url = url.replace('{{LNG}}', longitude)
+  url = url.replace('{{RDS}}', 50)
+  url = url.replace('{{CNT}}', 1)
+
+  let response = await fetch('https://cors.io/?' + url, {
     method: 'GET',
     headers: new Headers({
       'Accept': 'application/json'
